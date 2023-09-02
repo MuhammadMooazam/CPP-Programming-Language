e888848cf2d0333dc73e4bf6f8015af351873246
Syed Muhammad Mooazam <muhammadmoazam29@gmail.com>
2023-09-02 11:32:54 +0500

    Add .cpp files

---
+++ 02)add.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5, b = 3;
+    cout << "Sum of " << a << " and " << b << " is " << a + b;
+    return 0;
+}
---
+++ 03)userInput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter first number: " << endl;
+    cin >> a;
+    cout << "Enter second number: " << endl;
+    cin >> b;
+    cout << "First number is " << a << " and second number is " << b;
+    return 0;
+}
